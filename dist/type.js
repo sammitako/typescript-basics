bb39b2b7163fbb5a4f29fe71c9ca1cd162ddb1ae
sammitako <p.hansaem@hotmail.com>
2022-03-31 20:45:30 +0900

    Type Practice

---
+++ dist/type.js
@@ -0,0 +1,14 @@
+"use strict";
+const message = "hello typescript";
+console.log(message);
+let count = 0;
+count += 1;
+// count = '문자열'
+const done = false;
+const numbers = [1, 2, 3];
+const messages = ["hello", "world"];
+// messages.push(1);
+let mightBeUndefined = undefined;
+let nullableNumber = null;
+let color = "red";
+// color ='green';
---
+++ src/type.ts
@@ -0,0 +1,17 @@
+const message: string = "hello typescript";
+console.log(message);
+
+let count = 0;
+count += 1;
+// count = '문자열'
+
+const done: boolean = false;
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ["hello", "world"];
+// messages.push(1);
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: "red" | "orange" | "yellow" = "red";
+// color ='green';
